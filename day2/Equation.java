87df4b27b0e4ecfcd483de70337c54077b189fd8
NNQA <anhla12h@gmail.com>
2025-04-04 12:15:31 +0700

    add resolve p21 Equation problem

---
+++ day2/Equation.java
@@ -0,0 +1,12 @@
+package day2;
+
+import java.util.Scanner;
+public class Equation {
+    static Scanner sc = new Scanner(System.in); 
+
+ 
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        System.out.println(9*n + " " + 8*n);
+    }
+}
