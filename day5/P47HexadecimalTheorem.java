7daa3a7f36dd56feebc23964ff24cfeaa402ad4e
NNQA <anhla12h@gmail.com>
2025-08-29 14:27:34 +0700

    add resolve P47  solutions

---
+++ day5/P47HexadecimalTheorem.java
@@ -0,0 +1,16 @@
+package day5;
+
+import java.util.Scanner;
+
+public class P47HexadecimalTheorem {
+    public static void main(String[] args) {
+        Scanner i = new Scanner(System.in);
+        int a = i.nextInt();
+    
+
+        System.out.println(0);
+        System.out.println(0);
+        System.out.println(a);
+
+    }
+}
